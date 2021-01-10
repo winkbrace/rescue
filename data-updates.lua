fdeb0bd40db53c13d18f57e8ebf0cfd8726879a9
Bas de Ruiter <deruiterb@gmail.com>
2021-01-10 21:47:31 +0100

    Initial commit. Disable advanced assemblers, miners and furnaces

---
+++ data-updates.lua
@@ -0,0 +1,50 @@
+local Log = require("__stdlib__/stdlib/misc/logger").new("data-updates")
+local Recipe = require("__stdlib__/stdlib/data/recipe")
+
+-- These are the basic assembling entities that are allowed. Any more advanced will be removed
+-- We use the trick of using the values as key so we can use entities_to_keep[entity] in an if condition
+local entities_to_keep = {
+    ["assembling-machine-1"] = true,
+    ["oil-refinery"] = true,
+    ["chemical-plant"] = true,
+    ["rocket-silo"] = true,
+    ["burner-mining-drill"] = true,
+    ["pumpjack"] = true, -- I'm scared of trying to make the drones mine fluids
+    ["mining-depot"] = true,
+    ["stone-furnace"] = true,
+}
+-- We collect the entities in "removed_entities" so we know what technologies to remove afterwards
+local removed_entities = {}
+
+-- Remove all advanced assemblers, miners and smelters
+for _,type in ipairs({"assembling-machine", "mining-drill", "furnace"}) do
+    for entity in pairs(data.raw[type]) do
+        if entities_to_keep[entity] then
+            data.raw[type][entity].next_upgrade = nil
+        else
+            Log.log("Entity removed by Rescue: " .. entity)
+            data.raw[type][entity] = nil
+            data.raw.item[entity] = nil
+            data.raw.recipe[entity] = nil
+            removed_entities[entity] = true
+        end
+    end
+end
+
+-- Remove entity unlocks from technologies
+for key, technology in pairs(data.raw.technology) do
+    for i, effect in ipairs(technology.effects or {}) do
+        if effect.type == "unlock-recipe" and removed_entities[effect.recipe] then
+            table.remove(data.raw.technology[key].effects, i)
+        end
+    end
+end
+
+-- Adjust purple science recipe to require substation instead of electric furnace
+Recipe("production-science-pack"):replace_ingredient("electric-furnace", "substation")
+
+-- Remove tips & tricks that trigger when something related to our removed buildings happens
+data.raw["tips-and-tricks-item"]["fast-replace"] = nil
+data.raw["tips-and-tricks-item"]["fast-replace-belt-splitter"] = nil
+data.raw["tips-and-tricks-item"]["fast-replace-belt-underground"] = nil
+data.raw["tips-and-tricks-item"]["rotating-assemblers"] = nil
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Rescue
+
+Rescue is a mod for the game Factorio.
+
+## Story
+
+You came to this planet in response to a rescue signal. Unfortunately you were shot down as you approached.
+Luckily you survived the crash and now it's up to you to save those poor souls that are still hiding out there.
+
+## Gameplay
+
+In this mod you don't have access to anything more advanced than burner mining drills and basic assemblers. There are no ore patches outside your starting area.
+You have to venture out to rescue colonist outposts. Once rescued these colonists will supply you with a steady stream of resources.
+As you rescue an outpost, another one will be randomly generated a little furhter away than the previous one. The more outposts you
+rescue, the more advanced materials they will provide.
+
+> Note: Maybe we require the user to take a minimum amount (1000?) of the resource before we generate the next outpost.
+
+The idea is that you are encouraged to focus on building a good military and train network early on.
+
+
+
+## Wishes
+
+- The colonists are mining drones and they spawn next to a small ore patch of advanced materials.
+- Balance the game so that players still need to build starter base small quantities of everything,
+  but they don't have to upscale after that.
+- Compatibility with Krastorio 2
+- Compatibility with Bob's
